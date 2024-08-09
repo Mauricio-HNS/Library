cc42934bf959ac7b2d9978726276cb4945f36652
Mauricio-HNS <mauricio-developer@outlook.com>
2024-08-09 14:17:22 -0300

    04
    Criação das Controllers e ajustes

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Biblioteca.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
